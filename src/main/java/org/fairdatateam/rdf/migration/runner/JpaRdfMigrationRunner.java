d4f99220052ce61653397ddd5c96b32b3c4900f3
dennisvang <29799340+dennisvang@users.noreply.github.com>
2024-11-21 10:47:27 +0100

    Q&D implementation of JpaRdfMIgrationRunner as copy of MongoRdfMigrationRunner
    lots of duplicated code, but this is easiest due to coupling with repository and entity
    
    todo: DRY up

---
+++ src/main/java/org/fairdatateam/rdf/migration/runner/JpaRdfMigrationRunner.java
@@ -0,0 +1,110 @@
+/*
+ * The MIT License
+ * Copyright © 2019 DTL
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.fairdatateam.rdf.migration.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fairdatateam.rdf.migration.database.RdfMigrationCrudRepository;
+import org.fairdatateam.rdf.migration.entity.JpaRdfMigration;
+import org.fairdatateam.rdf.migration.entity.RdfMigrationAnnotation;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * The class contains a logic about executing the migration
+ *
+ * @author Vojtech Knaisl (vknaisl)
+ * @since 1.0.0
+ */
+@Slf4j
+public class JpaRdfMigrationRunner {
+
+    /**
+     * A repository for storing information about metadata into a database
+     */
+    private RdfMigrationCrudRepository rdfMigrationRepository;
+
+    /**
+     * A Spring application context that is needed to retrieve a concrete annotated migration class defined by a user
+     */
+    private ApplicationContext appContext;
+
+    public JpaRdfMigrationRunner(RdfMigrationCrudRepository rdfMigrationRepository,
+                                        ApplicationContext appContext) {
+        this.rdfMigrationRepository = rdfMigrationRepository;
+        this.appContext = appContext;
+    }
+
+    /**
+     * The method executes a migration process. It loads all migrations from the Spring Application context, filters
+     * them with already completed annotation, and runs the migrations that weren't run yet.
+     */
+    public void run() {
+        log.info("Migration of RDF Store started");
+        Iterable<JpaRdfMigration> migrationsInDB = rdfMigrationRepository.findAll();
+        Stream<JpaRdfMigration> migrations = StreamSupport.stream(migrationsInDB.spliterator(), false);
+        int lastMigrationNumber = migrations
+                .map(JpaRdfMigration::getNumber)
+                .max(Integer::compareTo)
+                .orElse(0);
+
+        appContext.getBeansWithAnnotation(RdfMigrationAnnotation.class)
+                .values()
+                .stream()
+                .map(o -> {
+                    if (!(o instanceof Migratable)) {
+                        log.error("Defined Migration has to implement Migratable");
+                        return null;
+                    }
+                    return (Migratable) o;
+                })
+                .filter(Objects::nonNull)
+                .filter(m -> getAnnotation(m).number() > lastMigrationNumber)
+                .sorted(Comparator.comparingInt(m -> getAnnotation(m).number()))
+                .forEach(m -> {
+                    JpaRdfMigration mEntity = new JpaRdfMigration(getAnnotation(m).number(),
+                            getAnnotation(m).name(),
+                            getAnnotation(m).description());
+                    log.info("Migration (n. {}) started", mEntity.getNumber());
+                    m.runMigration();
+                    rdfMigrationRepository.save(mEntity);
+                    log.info("Migration (n. {}) ended", mEntity.getNumber());
+                });
+        log.info("Migration of RDF Store ended");
+    }
+
+    /**
+     * A helper method for retrieving an annotation from the provided object
+     *
+     * @param migration A migration from which we want to extract annotation
+     * @return The desired annotation
+     */
+    private RdfMigrationAnnotation getAnnotation(Migratable migration) {
+        return migration.getClass().getAnnotation(RdfMigrationAnnotation.class);
+    }
+
+}
